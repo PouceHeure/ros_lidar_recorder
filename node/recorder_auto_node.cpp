1795ae597b5cb79b7829a9a6af6a415b2cb638e2
PouceHeure <hugo.pousseur@gmail.com>
2020-08-26 13:58:15 +0200

    add nodes parameters

--- node/recorder_auto_node.cpp
+++ node/recorder_auto_node.cpp
@@ -1,12 +1,21 @@
 #include <ros_lidar_recorder/recorder.hpp>
 
+#define PARAM_NAME_FREQ "freq"
+#define PARAM_NAME_PATH_DATA "path_data"
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "recorder_event");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
 
-  std::string path_dir_data = get_default_path_data();
-  RecorderAuto recorder_auto(nh,"scan",path_dir_data,10);
+  int param_freq = 10;
+  nh_private.getParam(PARAM_NAME_FREQ,param_freq);
+
+  std::string param_path_data = get_default_path_data();
+  nh_private.getParam(PARAM_NAME_PATH_DATA,param_path_data);  
+
+  ros::NodeHandle nh;
+  RecorderAuto recorder_auto(nh,"scan",param_path_data,param_freq);
 
   ros::spin();
 
--- node/recorder_event_node.cpp
+++ node/recorder_event_node.cpp
@@ -3,10 +3,14 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "recorder_event");
+  ros::NodeHandle nh_private("~");
+
+  std::string param_path_data = get_default_path_data();
+  nh_private.getParam(PARAM_NAME_PATH_DATA,param_path_data);  
+
   ros::NodeHandle nh;
 
-  std::string path_dir_data = get_default_path_data();
-  RecorderEvent recorder_event(nh,"scan",path_dir_data,"record_event");
+  RecorderEvent recorder_event(nh,"scan",param_path_data,"record_event");
 
   ros::spin();
 
